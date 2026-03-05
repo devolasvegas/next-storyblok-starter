afac7e5c8ee59a28bd33604cce28062c1b088985
Devon Daviau <devondaviau@yahoo.ca>
2026-03-05 15:02:05 -0500

    rejig and simplify the next.js caching strategy

--- app/[[...slug]]/page.jsx
+++ app/[[...slug]]/page.jsx
@@ -7,15 +7,17 @@ export default async function Page({ params }) {
   const fullSlug = slug ? slug.join("/") : "home";
 
   const storyblokApi = getStoryblokApi();
-  const sbCacheVersion = await storyblokApi.cacheVersion();
-
-  console.log("Cache version:", sbCacheVersion);
 
   const sbParams = {
     version: "published",
   };
 
-  const { data } = await storyblokApi.get(`cdn/stories/${fullSlug}`, sbParams);
+  // Pass the slug in the body of the request so it can be used in the custom fetch function to set cache tags for Next.js' Data Cache.
+  const { data } = await storyblokApi.get(`cdn/stories/${fullSlug}`, sbParams, {
+    body: {
+      slug: fullSlug,
+    },
+  });
 
   return <StoryblokStory story={data.story} />;
 }
--- app/lib/storyblok.js
+++ app/lib/storyblok.js
@@ -1,19 +1,9 @@
 import { apiPlugin, storyblokInit } from "@storyblok/react";
 
 import { Feature, Grid, Page, Teaser } from "../components/storyblok";
-import { get } from "http";
 
 const storyblokToken =
   process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_API_ACCESS_TOKEN;
-
-const storyblokSpaceCacheTag = "storyblok-space";
-
-// Retrieve the current cache version
-// async function getCv() {
-//   return await storyblokClient
-//     .get("cdn/spaces/me", { version: "published" })
-//     .then((x) => x.data.space.version);
-// }
 
 export const getStoryblokApi = storyblokInit({
   accessToken: storyblokToken,
@@ -21,38 +11,21 @@ export const getStoryblokApi = storyblokInit({
   apiOptions: {
     // Creating custom fetch function to set work with the Next.js Data Cache
     // We want to limit requests to Storyblok so the cache is renewed only when new changes have been made to the content.
-    fetch: async (urlOrRequest, things) => {
-      console.log("things", things);
-
+    fetch: async (urlOrRequest, init) => {
       const url = new URL(
         typeof urlOrRequest === "string" || urlOrRequest instanceof URL
           ? urlOrRequest
           : urlOrRequest.url,
       );
 
-      if (url.pathname === "/v2/cdn/spaces/me") {
-        /**
-         * Never put the cache version in the request url for the space,
-         * as we use this request to get the up to date cache version.
-         */
-        url.searchParams.delete("cv");
+      // Slug should be passed in via the custom fetch function's init.body.
+      const slug = init?.body ? init.body.slug : "";
 
-        return fetch(url.toString(), {
-          next: {
-            tags: [storyblokSpaceCacheTag],
-            revalidate: false,
-          },
-        });
-      }
-
-      // url.searchParams.set("cv", (await getCv()).toString());
-
+      // Creating a cache tag for each Storyblok slug, so we can invalidate the cache for specific pages when they are updated in Storyblok.
       return fetch(url.toString(), {
-        next: { revalidate: false },
+        next: { tags: [`story:${slug}`] },
       });
     },
-    rateLimit: 100000,
-    cache: { type: "none" },
     region: "eu",
   },
   components: {
