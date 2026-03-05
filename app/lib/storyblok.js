2267b11244972808e43f91b583e4972128414587
Devon Daviau <devondaviau@yahoo.ca>
2026-03-05 12:16:36 -0500

    update storyblok.js for some debugging

--- app/lib/storyblok.js
+++ app/lib/storyblok.js
@@ -2,6 +2,11 @@ import { apiPlugin, storyblokInit } from "@storyblok/react";
 
 import { Feature, Grid, Page, Teaser } from "../components/storyblok";
 
+const storyblokToken =
+  process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_API_ACCESS_TOKEN;
+
+console.log("Storyblok Token:", storyblokToken);
+
 export const getStoryblokApi = storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_API_ACCESS_TOKEN,
   use: [apiPlugin],
