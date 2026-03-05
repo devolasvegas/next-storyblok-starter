5ea6fa84ad94d434015f38716a312819afff6667
Devon Daviau <devondaviau@yahoo.ca>
2026-03-05 13:49:46 -0500

    try to config caching in storyblok.js and page.js

--- app/[[...slug]]/page.jsx
+++ app/[[...slug]]/page.jsx
@@ -6,11 +6,15 @@ export default async function Page({ params }) {
 
   const fullSlug = slug ? slug.join("/") : "home";
 
+  const storyblokApi = getStoryblokApi();
+  const sbCacheVersion = await storyblokApi.cacheVersion();
+
+  console.log("Cache version:", sbCacheVersion);
+
   const sbParams = {
     version: "published",
   };
 
-  const storyblokApi = getStoryblokApi();
   const { data } = await storyblokApi.get(`cdn/stories/${fullSlug}`, sbParams);
 
   return <StoryblokStory story={data.story} />;
