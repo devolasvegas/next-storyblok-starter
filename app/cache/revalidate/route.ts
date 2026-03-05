303376cab4c6feb5d4c95fa4956be0cd27bc8122
Devon Daviau <devondaviau@yahoo.ca>
2026-03-05 13:31:57 -0500

    add some comments to the cache revalidation route

--- app/cache/revalidate/route.ts
+++ app/cache/revalidate/route.ts
@@ -1,9 +1,13 @@
 import { revalidateTag } from "next/cache";
 
+/*
+ We are using this function to revalidate the next cache to strictly limit the number of requests to Storyblok. The cache will only be renewed when new changes have been made to the content in Storyblok, which is determined by the cache tag "storyblok-space" that we set in the custom fetch function in storyblok.js. This way we can ensure that the cache is only renewed when necessary, and not on every page request.
+*/
 export async function POST(request: Request) {
   const url = new URL(request.url);
   const cacheTag: string | null = url.searchParams.get("cacheTag");
 
+  // Making this more complex than needed right now just in case we want to be able to have multiple cache tags down the road.
   const validCacheTags = ["storyblok-space"];
   const isValidCacheTag = validCacheTags.includes(cacheTag || "");
 
