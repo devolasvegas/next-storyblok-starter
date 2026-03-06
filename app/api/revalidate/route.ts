9331de09d40644bebf1c0acaf34db85b2aa5672a
Devon Daviau <devondaviau@yahoo.ca>
2026-03-06 11:15:30 -0500

    in revalidate route handler, change cache tag prefix from story to storyblok

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -6,7 +6,7 @@ import { revalidateTag } from "next/cache";
 export async function POST(request: Request) {
   const body = await request.json();
   const storyId = body.story_id;
-  const cacheTag: string | null = storyId ? `story:${storyId}` : null;
+  const cacheTag: string | null = storyId ? `storyblok:${storyId}` : null;
 
   console.log(`Received revalidation request for cacheTag: ${cacheTag}`);
 
